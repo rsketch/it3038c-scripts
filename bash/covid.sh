1dd455ad0e1de2a0cec4b3f3101408813af14e48
rsketch <sketchrn@mail.uc.edu>
2023-01-17 22:55:46 -0500

    Lab 2

--- bash/covid.sh
+++ bash/covid.sh
@@ -8,8 +8,10 @@ DEATH=$(echo $DATA | jq '.[0].death')
 
 NEGATIVE=$(echo $DATA | jq '.[0].negative')
 
+CURNTHOSPITALIZED=$(echo $DATA | jq '.[0].hospitalizedCurrently')
+
 #todays date
 TODAY=$(date)
 #data that is printed out
-echo "On $TODAY, there were $POSITIVE positive COVID cases, $DEATH deaths, and $NEGATIVE negative COVID tests. "
+echo "On $TODAY, there were $POSITIVE positive COVID cases, $DEATH deaths, $NEGATIVE negative COVID tests, and $CURNTHOSPITALIZED currently hospitalized."
 
